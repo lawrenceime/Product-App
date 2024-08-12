228a99c24d628ef3a0974c3e5de61a95bdca088d
lawrhymes <lawrenceime246@gmail.com>
2024-08-12 18:52:30 +0100

    I removed the hero section for mobile screens

--- app/component/Hero.tsx
+++ app/component/Hero.tsx
@@ -5,7 +5,7 @@ import { FaArrowRight } from "react-icons/fa6";
 const HeroSection = () => {
   return (
 
-       <div className=  '  relative w-[90vw]  h-[350px] mx-auto mt-[24px] rounded-[12px] ' >
+       <div className=  'hidden md:block lg:block relative w-[90vw]  h-[350px] mx-auto mt-[24px] rounded-[12px] ' >
 
     <div className='  rounded-[12px] overflow-hidden'>
         <Image src='https://images.pexels.com/photos/1670766/pexels-photo-1670766.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' fill className='object-cover object-center opacity-[60%]' alt='White sneakers'/>
@@ -22,5 +22,5 @@ const HeroSection = () => {
   
   )
 }
-
+// Cycle through images if there are more products than images
 export default HeroSection
